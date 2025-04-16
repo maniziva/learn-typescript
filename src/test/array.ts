9e4c21fd766f1684ca1c59f22cc24363daf724c2
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-04-16 13:28:40 +0530

    update: array-1

---
+++ src/test/array.ts
@@ -0,0 +1,11 @@
+let arr : number[] = [1, 3, 4, 5, 2];
+console.log("Without order: ", arr);
+
+arr.sort((a, b) => a - b);
+console.log("Ascending Order: ", arr);
+
+arr.sort((a, b) => b - a);
+console.log("Descending Order: ", arr);
+
+console.log("First element: ", arr[0]);
+console.log("Last element: ", arr[arr.length - 1]);
