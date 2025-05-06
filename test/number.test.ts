adf571a02a7dff2537faed4d391236d6084e04ca
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-06 12:10:10 +0530

    update: factorial

--- test/number.test.ts
+++ test/number.test.ts
@@ -9,3 +9,14 @@ test('Get Date and Time', () => {
     console.log(currentDateTime); // 2025-04-28 10:26:45
 
 });
+
+test('Find factorial of a number', () => {
+    const num: number = 5;
+    let factorial: number = 1;
+    
+    for (let i = 1; i <= num; i++) {
+        factorial = factorial * i;
+        }
+    
+    console.log(`Factorial of ${num} is ${factorial}`);
+});
