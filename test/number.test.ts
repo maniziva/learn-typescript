f190570e749fa983f7a3c49f487d9a8f88e9da83
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-16 13:21:32 +0530

    update: date function

--- test/number.test.ts
+++ test/number.test.ts
@@ -20,3 +20,27 @@ test('Find factorial of a number', () => {
     
     console.log(`Factorial of ${num} is ${factorial}`);
 });
+
+test('without dayjs lib', ()=>{
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+
+    console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}`);
+    console.log(`${year}-${month}-${day} ${hour}:${minute}:${second}`);
+    console.log(`${year}${month}${day}${hour}${minute}${second}`);
+    console.log(`${year}${month}${day}`);
+    console.log(`${year}${month}${day}${hour}${minute}${second}`);
+
+    // Need month or date if single digit it shoutld be 0
+    const formattedMonth = month < 10 ? `0${month}` : month;
+    const formattedDate = day < 10 ? `0${day}` : day;
+    const formattedHour = hour < 10 ? `0${hour}` : hour;
+    const formattedMinute = minute < 10 ? `0${minute}` : minute;
+    const formattedSecond = second < 10 ? `0${second}` : second;
+    console.log(`${year}${formattedMonth}${formattedDate}${formattedHour}${formattedMinute}${formattedSecond}`);
+});
