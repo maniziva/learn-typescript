2f280d946a96dc298d126bf4a93e7f9e38166168
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-25 14:09:09 +0530

    update: sort

--- test/array/sort.test.ts
+++ test/array/sort.test.ts
@@ -18,3 +18,31 @@ test("Find Largest/Smallest - Sort", () => {
   console.log("Largest Number: ", arr[arr.length - 1]);
   console.log("second largest number: ", arr[arr.length - 2]);
 });
+
+test("without sort function", () => {
+  const arr = [10, 2, 8, 7, 6, 3, 9, 1, 4, 5];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        // Swap elements
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  console.log(arr); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+});
+
+test("Sort with custom object", () => {
+  const arr = [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 22 },
+    { name: "Doe", age: 30 },
+  ];
+
+  arr.sort((a, b) => a.age - b.age);
+  console.log(arr); // Sorted by age in ascending order
+});
