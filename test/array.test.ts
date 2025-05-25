63d9373cf33fa41f3c85c528131adff28216019f
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-25 10:13:43 +0530

    Find second largest number in an array - using sort and slice'

--- test/array.test.ts
+++ test/array.test.ts
@@ -324,3 +324,13 @@ test('Count repeated characters in a string array', () => {
   const repeatedChars = Object.entries(charCount).filter(([char, count]) => count > 1);
   console.log(repeatedChars);
 });
+
+test('Find second largest number in an array - using sort and slice', () => {
+  const arr: number[] = [1, 2, 3, 4, 5, 8];
+  console.log("Without order: ", arr);
+
+  const sortedArr = arr.sort((a, b) => a - b);
+  const secondLargest = sortedArr.slice(-2, -1);
+  console.log("Second largest number: ", secondLargest); // Output: 5
+}
+);
