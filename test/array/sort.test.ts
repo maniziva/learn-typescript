d9e78b6288a246b45a4572f0ace2ddd9acd24ba3
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-05-26 11:27:59 +0530

    update: sort

--- test/array/sort.test.ts
+++ test/array/sort.test.ts
@@ -47,9 +47,55 @@ test("Sort with custom object", () => {
   console.log(arr); // Sorted by age in ascending order
 });
 
-
-test('Sort with spread Operator', () => {
+test("Sort with spread Operator", () => {
   const arr = [10, 2, 8, 7, 6, 3, 9, 1, 4, 5];
   const sortedArr = [...arr].sort((a, b) => a - b);
   console.log(sortedArr); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+});
+
+test("Sort with localeCompare", () => {
+  const arr = [
+    "Banana",
+    "apple",
+    "Cherry",
+    "Adaikalam",
+    "123",
+    "!",
+    "#",
+    "zebra",
+    "*Mani",
+  ];
+  arr.sort((a, b) => a.localeCompare(b));
+  console.log(arr); // ["apple", "banana", "cherry"]
+});
+
+test("Sort with localeCompare - case insensitive", () => {
+  const arr = [
+    "Banana",
+    "apple",
+    "Cherry",
+    "Adaikalam",
+    "123",
+    "!",
+    "#",
+    "zebra",
+    "*Mani",
+  ];
+  arr.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: "base" }));
+  console.log(arr); // ["!", "#", "*Mani", "123", "Adaikalam", "apple", "Banana", "Cherry", "zebra"]
+});
+
+test("Sort by using functions", () => {
+  const arr: number[] = [10, 2, 8, 7, 6, 3, 9, 1, 4, 5];
+
+  function ascending(arr: number[]): number[] {
+    return arr.sort((a, b) => a - b);
+  }
+
+  function descending(arr: number[]): number[] {
+    return arr.sort((a, b) => b - a);
+  }
+
+  console.log("Ascending Order: ", ascending(arr));
+  console.log("Descending Order: ", descending(arr));
 });
