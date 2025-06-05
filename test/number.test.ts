19bdc8673cce6e5b17d69c553c5ea4944d550bf4
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-05 22:07:15 +0530

    update: numbers

--- test/number.test.ts
+++ test/number.test.ts
@@ -1,48 +1,77 @@
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
-test('Get Date and Time', () => {
+test("Get Date and Time", () => {
+  const currentDate = dayjs().format("YYYYMMDDHHmmss");
+  console.log(currentDate); // 2025-04-28
 
-    const currentDate = dayjs().format('YYYYMMDDHHmmss');
-    console.log(currentDate); // 2025-04-28
-    
-    const currentDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
-    console.log(currentDateTime); // 2025-04-28 10:26:45
-
+  const currentDateTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  console.log(currentDateTime); // 2025-04-28 10:26:45
 });
 
-test('Find factorial of a number', () => {
-    
-    function factorial(num: number) {
-        let fact : number = 1;
-        for(let i = 1; i<=num; i++){
-            fact *= i;
-        }
-        return fact;
+test("Find factorial of a number", () => {
+  function factorial(num: number) {
+    let fact: number = 1;
+    for (let i = 1; i <= num; i++) {
+      fact *= i;
     }
-    
-    console.log(factorial(5));
+    return fact;
+  }
+
+  console.log(factorial(5));
 });
 
-test('without dayjs lib', ()=>{
+test("Find factorial of a number array", () => {
+  function factorial(n: number) {
+    let fact: number = 1;
+    for (let i = 1; i <= n; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
+
+  const numbers = [3, 4, 5];
+  const result = numbers.map(factorial);
+
+  console.log(result);
+});
+
+test("without dayjs lib", () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+
+  console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}`);
+  console.log(`${year}-${month}-${day} ${hour}:${minute}:${second}`);
+  console.log(`${year}${month}${day}${hour}${minute}${second}`);
+  console.log(`${year}${month}${day}`);
+  console.log(`${year}${month}${day}${hour}${minute}${second}`);
+
+  // Need month or date if single digit it shoutld be 0
+  const formattedMonth = month < 10 ? `0${month}` : month;
+  const formattedDate = day < 10 ? `0${day}` : day;
+  const formattedHour = hour < 10 ? `0${hour}` : hour;
+  const formattedMinute = minute < 10 ? `0${minute}` : minute;
+  const formattedSecond = second < 10 ? `0${second}` : second;
+  console.log(
+    `${year}${formattedMonth}${formattedDate}${formattedHour}${formattedMinute}${formattedSecond}`
+  );
+});
+
+test("Get birth year of given number array", () => {
+  function birthYear(n: number) {
     const now = new Date();
     const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const second = now.getSeconds();
 
-    console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}`);
-    console.log(`${year}-${month}-${day} ${hour}:${minute}:${second}`);
-    console.log(`${year}${month}${day}${hour}${minute}${second}`);
-    console.log(`${year}${month}${day}`);
-    console.log(`${year}${month}${day}${hour}${minute}${second}`);
+    let age: number = year - n;
+    return age;
+  }
+  const numbers = [1996, 2003, 2004, 2005];
 
-    // Need month or date if single digit it shoutld be 0
-    const formattedMonth = month < 10 ? `0${month}` : month;
-    const formattedDate = day < 10 ? `0${day}` : day;
-    const formattedHour = hour < 10 ? `0${hour}` : hour;
-    const formattedMinute = minute < 10 ? `0${minute}` : minute;
-    const formattedSecond = second < 10 ? `0${second}` : second;
-    console.log(`${year}${formattedMonth}${formattedDate}${formattedHour}${formattedMinute}${formattedSecond}`);
+  numbers.forEach((year) => {
+    console.log(`Born in ${year} → Age: ${birthYear(year)}`);
+  });
 });
