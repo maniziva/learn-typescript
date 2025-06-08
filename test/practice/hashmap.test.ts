2b94e5ead3221bf69b270928c8ef68dccc51fb5a
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-08 18:38:26 +0530

    update: check if two string are anagram

--- test/practice/hashmap.test.ts
+++ test/practice/hashmap.test.ts
@@ -103,3 +103,32 @@ test("Find the frequency of each word in a sentence", () => {
 
   console.log(wordCount);
 });
+
+
+test('check if two string are anagram', ()=>{
+  function isAnagram(str1: string, str2: string): boolean {
+    if (str1.length !== str2.length) return false;
+  
+    const count1: { [key: string]: number } = {};
+    const count2: { [key: string]: number } = {};
+  
+    for (const char of str1) {
+      count1[char] = (count1[char] || 0) + 1;
+    }
+  
+    for (const char of str2) {
+      count2[char] = (count2[char] || 0) + 1;
+    }
+  
+    const sortedCount1 = Object.entries(count1).sort();
+    const sortedCount2 = Object.entries(count2).sort();
+  
+  
+    return JSON.stringify(sortedCount1) === JSON.stringify(sortedCount2);
+  }
+  
+  const str1: string = "anim";
+  const str2: string = "mani";
+  
+  console.log(isAnagram(str1, str2)); // ✅ true
+})
