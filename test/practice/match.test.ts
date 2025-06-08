0822a434b9428a7eecb409e23b483474c472748a
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-08 17:49:21 +0530

    update: To find vowels, consonants, spaces, special characters in a string

--- test/practice/match.test.ts
+++ test/practice/match.test.ts
@@ -15,3 +15,24 @@ test("Regular expression to find String", () => {
   const matches: string[] = str.match(regex) || [];
   console.log("Matches: ", matches[1]);
 });
+
+test("To find vowels, consonants, spaces, digits, special characters in a string", () => {
+  const str: string = "Manik andan Adaik alam-123";
+  console.log(`String length: ${str.length}`);
+
+  const spaces = str.match(/\s/g) || [];
+  console.log(`Number of spaces: ${spaces.length}`);
+
+  const vowels = str.match(/[aeiou]/gi) || [];
+  console.log(`Number of vowels: ${vowels.length}`);
+
+  const consonants =
+    str.match(/[a-z]/gi)?.filter((item) => !/[aeiou]/i.test(item)) || [];
+  console.log(`Number of consonants: ${consonants.length}`);
+
+  const digits = str.match(/\d/g) || [];
+  console.log(`Number of digits: ${digits.length}`);
+
+  const specialChar = str.match(/[^a-z0-9\s]/gi) || [];
+  console.log(`Number of special characters: ${specialChar.length}`);
+});
