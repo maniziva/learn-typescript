37cd6b64f9d83ab364eda7b2211a85dbdb208d42
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-08 17:20:31 +0530

    update: replace

---
+++ test/practice/replace.test.ts
@@ -0,0 +1,11 @@
+test('Remove all whitsepaces from a string', ()=>{
+    const str: string = "Manikandan Adaikalam";
+    const removedStr = str.replace(/\s+/g, "");
+    console.log(removedStr);
+});
+
+test('Replace the string', ()=>{
+    const str: string = "Manikandan Adaikalam";
+    const replaceStr = str.replace(/a/gi, "A");
+    console.log(replaceStr);
+})
