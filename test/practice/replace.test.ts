80a35fa3c64731b95c9c19f5634f0bb1686ef180
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-08 17:22:13 +0530

    update: replace string array

--- test/practice/replace.test.ts
+++ test/practice/replace.test.ts
@@ -8,4 +8,10 @@ test('Replace the string', ()=>{
     const str: string = "Manikandan Adaikalam";
     const replaceStr = str.replace(/a/gi, "A");
     console.log(replaceStr);
-})
+});
+
+test('Replace the string array', ()=>{
+    const str: string[] = ["Manikandan", "Adaikalam"];
+    const replaceStr = str.map(item => item.replace(/a/gi, "A"));
+    console.log(replaceStr);
+});
