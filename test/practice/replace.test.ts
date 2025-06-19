8bd5b9cec99da0c539b6dbfe01a976fc2fd9ca82
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-06-19 11:24:10 +0530

    update: remove white space

--- test/practice/replace.test.ts
+++ test/practice/replace.test.ts
@@ -1,17 +1,19 @@
-test('Remove all whitsepaces from a string', ()=>{
-    const str: string = "Manikandan Adaikalam";
-    const removedStr = str.replace(/\s+/g, "");
-    console.log(removedStr);
+test("Remove all whitsepaces from a string", () => {
+  const str: string = "Manikandan Adaikalam";
+  const removedStr = str.replace(/\s+/g, "");
+  console.log(removedStr);
+  const remove = str.split(" ").join("");
+  console.log(remove);
 });
 
-test('Replace the string', ()=>{
-    const str: string = "Manikandan Adaikalam";
-    const replaceStr = str.replace(/a/gi, "A");
-    console.log(replaceStr);
+test("Replace the string", () => {
+  const str: string = "Manikandan Adaikalam";
+  const replaceStr = str.replace(/a/gi, "A");
+  console.log(replaceStr);
 });
 
-test('Replace the string array', ()=>{
-    const str: string[] = ["Manikandan", "Adaikalam"];
-    const replaceStr = str.map(item => item.replace(/a/gi, "A"));
-    console.log(replaceStr);
+test("Replace the string array", () => {
+  const str: string[] = ["Manikandan", "Adaikalam"];
+  const replaceStr = str.map((item) => item.replace(/a/gi, "A"));
+  console.log(replaceStr);
 });
