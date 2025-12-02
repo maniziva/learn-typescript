7a3e3a1921129b2569415aee434315d71a07f4dd
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-12-02 18:11:12 +0530

    update

--- test/practice/factorial.test.ts
+++ test/practice/factorial.test.ts
@@ -6,3 +6,16 @@ test("Find factorial of a number", () => {
 
   console.log(factorial(5));
 });
+
+test("Find factorial of a number - for loop", () => {
+  function factorial(num: number): number {
+    if (num < 0) throw new Error("Given input is negative");
+    let fact: number = 1;
+    for (let i = 1; i <= num; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
+
+  console.log(factorial(5));
+});
