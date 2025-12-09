ff54d56efec358f1176b3be9706df0fb36467db1
Manikandan Adaikalam <manizivamsd@gmail.com>
2025-12-09 10:53:58 +0530

    Find first/second repeated and non-repeated character in a string - using hashmap

--- test/practice/duplicate.test.ts
+++ test/practice/duplicate.test.ts
@@ -40,3 +40,34 @@ test("Find Duplicate elements - using hashmap", () => {
   );
   console.log(duplicate);
 });
+
+
+test('Find first/second repeated and non-repeated character in a string - using hashmap', () => {
+  const str: string = "Manikandan";
+  const charCount: { [key: string]: number } = {};
+
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  console.log(charCount);
+  
+  // use find method to get the first non-repeated character
+  const firstNonRepeated = str.split("").find((char) => charCount[char] === 1);
+  const firstRepeated = str.split("").find((char) => charCount[char] > 1);
+
+  const repeated = str.split("").filter((char) => charCount[char] > 1);
+  const nonRepeated = str.split("").filter((char) => charCount[char] === 1);
+
+  console.log(firstNonRepeated);
+  console.log(firstRepeated);
+  console.log(repeated);
+  console.log(nonRepeated);
+
+  //find second non-repeated character
+  const secondNonRepeated = str.split("").find((char) => charCount[char] === 1 && char !== firstNonRepeated);
+  console.log(secondNonRepeated);
+
+  //find second repeated character
+  const secondRepeated = str.split("").find((char) => charCount[char] > 1 && char !== firstRepeated);
+  console.log(secondRepeated);
+});
